9f4c6de1b51cdaf4aafdf4a489ee4c0a858697dc
Kassukas123 <kaspar.einsok@gmail.com>
2024-09-15 21:50:30 +0300

    typo fix v6i GH harjutamine

--- 2024-09-09/god-tier/src/components/Homework.js
+++ 2024-09-09/god-tier/src/components/Homework.js
@@ -2,7 +2,7 @@ import React from "react"
 import "./Homework.css"
 
 const Homework = props => {
-  const hobbies = ["autoga sõitmine", "filmide vaatamine", "õues kimine"]
+  const hobbies = ["autoga sõitmine", "filmide vaatamine", "õues käimine"]
 
   return (
     <div>

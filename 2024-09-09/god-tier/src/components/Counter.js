219c4ab67ce3b1517135771bbd3b6c8c9ce20b19
Kassukas123 <kaspar.einsok@gmail.com>
2024-09-15 20:20:23 +0300

    kodut66 4 + promise n2ide

--- 2024-09-09/god-tier/src/components/Counter.js
+++ 2024-09-09/god-tier/src/components/Counter.js
@@ -3,19 +3,26 @@ import React, { useState } from "react"
 const Counter = () => {
   const [counter, setCounter] = useState(0)
 
-  const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  //const modifyCounter = () => setCounter(prevCounter => prevCounter + 1)
+  const modifyCounter = value => setCounter(prevCounter => prevCounter + value)
 
   return (
     <>
       <h1> {counter} </h1>
 
       {[+1, +5, +50, -1, -5, -50].map(element => (
-        <button onClick={modifyCounter}>sync {element}</button>
+        <button
+          key={element}
+          onClick={() => modifyCounter(element)}
+        >
+          nupp {element}
+        </button>
+        // <button onClick={modifyCounter}>sync {element}</button>
       ))}
 
-      <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
+      {/* <button onClick={() => setTimeout(() => modifyCounter(), 2000)}>
         async+1
-      </button>
+      </button> */}
     </>
   )
   //pane v2ikse t2hega, muidu viitad componendile, mitte muutajale
